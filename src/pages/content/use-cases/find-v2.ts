4ebe4e4a832aad243136d9018100bda55dbd943c
Ernest <iendeavor.github.io@gmail.com>
2024-04-03 17:47:57 +0800

    debug

--- src/pages/content/use-cases/find-v2.ts
+++ src/pages/content/use-cases/find-v2.ts
@@ -37,21 +37,28 @@ const find: Find = ({
     shouldMatchWholeWord,
     shouldUseRegularExpression,
   })
+  console.debug('[chrome-extension] [find] regex', regex)
 
   const nodeWithInnerTextList = createNodeWithInnerTextList({
     body: document.body,
   })
+  console.debug(
+    '[chrome-extension] [find] nodeWithInnerTextList',
+    nodeWithInnerTextList,
+  )
 
   const searchStringList = createSearchStringList({
     regex,
     innerText: document.body.innerText,
   })
+  console.debug('[chrome-extension] [find] searchStringList', searchStringList)
 
   const rangesList = createRangesList({
     nodeWithInnerTextList: nodeWithInnerTextList,
     searchStringList,
     shouldMatchWholeWord,
   })
+  console.debug('[chrome-extension] [find] rangesList', rangesList)
 
   match({
     rangesList,
