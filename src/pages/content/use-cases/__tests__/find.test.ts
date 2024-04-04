a543b63c837d4fda7835cce8c4e0461d53b1d68d
Ernest <iendeavor.github.io@gmail.com>
2024-04-04 12:39:16 +0800

    test: createNodeWithInnerTextList - rearrange code

--- src/pages/content/use-cases/__tests__/find.test.ts
+++ src/pages/content/use-cases/__tests__/find.test.ts
@@ -177,11 +177,11 @@ describe('createNodeWithInnerTextList', () => {
       name: 'should ignore <datalist>',
       param: {
         documentElement: createDocumentElement(
-          `
-<label for="ice-cream-choice">Choose a flavor:</label>
-<input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice"/>
-<datalist id="ice-cream-flavors"><option value="Chocolate"></option></datalist>
-          `.trim(),
+          [
+            `<label for="ice-cream-choice">Choose a flavor:</label>`,
+            `<input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice"/>`,
+            `<datalist id="ice-cream-flavors"><option value="Chocolate"></option></datalist>`,
+          ].join('\n'),
         ),
       },
       returnValue: [
