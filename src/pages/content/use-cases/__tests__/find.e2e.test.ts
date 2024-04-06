58afa29f8a6a7fe0a27526b967a1f0ece3cd806a
Ernest <iendeavor.github.io@gmail.com>
2024-04-06 11:56:04 +0800

    test: find

--- src/pages/content/use-cases/__tests__/find.e2e.test.ts
+++ src/pages/content/use-cases/__tests__/find.e2e.test.ts
@@ -17,19 +17,114 @@ type Suit = {
 const suits: Suit[] = []
 
 {
-  const documentElement = createDocumentElement(`a`)
+  const documentElement = createDocumentElement(
+    `<div id="browser-find-top-layer">a</div>`,
+  )
   suits.push({
     documentElement,
     text: 'a',
     shouldMatchCase: false,
     shouldMatchWholeWord: false,
     shouldUseRegularExpression: false,
-    expected: [
-      [createRange(documentElement.querySelector('body')!.childNodes[0], 0, 1)],
-    ],
+    expected: [],
   })
 }
 
+// === element start ====
+{
+  const documentElement = createDocumentElement(`<script>"a"</script>`)
+  suits.push({
+    documentElement,
+    text: 'a',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [],
+  })
+}
+{
+  const documentElement = createDocumentElement(`<noscript>a</noscript>`)
+  suits.push({
+    documentElement,
+    text: 'a',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [],
+  })
+}
+{
+  const documentElement = createDocumentElement(`<style>a {}</style>`)
+  suits.push({
+    documentElement,
+    text: 'a',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [],
+  })
+}
+{
+  const documentElement = createDocumentElement(
+    `<select><option>a</option></select>`,
+  )
+  suits.push({
+    documentElement,
+    text: 'a',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [],
+  })
+}
+// === element end ====
+
+// === class start ====
+{
+  const documentElement = createDocumentElement(
+    `<span class="sr-only">a</span>`,
+  )
+  suits.push({
+    documentElement,
+    text: 'a',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [],
+  })
+}
+// === class end ====
+
+// === style start ====
+{
+  const documentElement = createDocumentElement(
+    `<span style="display: none;">a</span>`,
+  )
+  suits.push({
+    documentElement,
+    text: 'a',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [],
+  })
+}
+{
+  const documentElement = createDocumentElement(
+    `<span style="visibility: hidden;">a</span>`,
+  )
+  suits.push({
+    documentElement,
+    text: 'a',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [],
+  })
+}
+// === style end ====
+
+// === match case start ====
 {
   const documentElement = createDocumentElement(`a`)
   suits.push({
@@ -67,7 +162,9 @@ const suits: Suit[] = []
     ],
   })
 }
+// === match case end ====
 
+// === match whole word start ====
 {
   const documentElement = createDocumentElement(`abc`)
   suits.push({
@@ -109,6 +206,53 @@ const suits: Suit[] = []
     ],
   })
 }
+// === match whole word end ====
+
+// === use regular expression start ====
+{
+  const documentElement = createDocumentElement(`\\d`)
+  suits.push({
+    documentElement,
+    text: '\\d',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [
+      [
+        createRange(documentElement.querySelector('body')!.childNodes[0], 0, 1),
+        createRange(documentElement.querySelector('body')!.childNodes[0], 1, 2),
+      ],
+    ],
+  })
+}
+{
+  const documentElement = createDocumentElement(`\\d`)
+  suits.push({
+    documentElement,
+    text: '\\d',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: true,
+    expected: [],
+  })
+}
+{
+  const documentElement = createDocumentElement(`\\d`)
+  suits.push({
+    documentElement,
+    text: '\\\\d',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: true,
+    expected: [
+      [
+        createRange(documentElement.querySelector('body')!.childNodes[0], 0, 1),
+        createRange(documentElement.querySelector('body')!.childNodes[0], 1, 2),
+      ],
+    ],
+  })
+}
+// === use regular expression end ====
 
 suits.forEach(
   ({
@@ -119,65 +263,70 @@ suits.forEach(
     shouldUseRegularExpression,
     expected,
   }) => {
-    const booleanPadMaxLength = 5
-    const textPadMaxLength = Math.max('text'.length, text.length) + 3
-    it(
-      [
-        'C'.padEnd(booleanPadMaxLength, ' '),
-        'W'.padEnd(booleanPadMaxLength, ' '),
-        'R'.padEnd(booleanPadMaxLength, ' '),
-        'text'.padEnd(textPadMaxLength, ' '),
-        'innerHTML',
-      ].join('  ') +
+    it(createTestName(), async () => {
+      // arrange
+      let onComplete = () => {}
+      const promise = new Promise<void>((r) => (onComplete = r))
+      const actual: Range[][] = []
+      const onNext = (ranges: Range[]) => actual.push(ranges)
+
+      // act
+      find({
+        documentElement,
+        text,
+        shouldMatchCase,
+        shouldMatchWholeWord,
+        shouldUseRegularExpression,
+        onNext,
+        onComplete,
+      })
+      await promise
+
+      // assert
+      try {
+        expect(actual.length).toBe(expected.length)
+
+        actual.forEach((actualRanges, rangesListIndex) => {
+          const expectedRanges = expected[rangesListIndex]
+          expect(actualRanges.length).toBe(expectedRanges.length)
+
+          actualRanges.forEach((actualRange, rangesIndex) => {
+            const expectedRange = expectedRanges[rangesIndex]
+            expect(actualRange.startContainer).toBe(
+              expectedRange.startContainer,
+            )
+            expect(actualRange.startOffset).toBe(expectedRange.startOffset)
+            expect(actualRange.endContainer).toBe(expectedRange.endContainer)
+            expect(actualRange.endOffset).toBe(expectedRange.endOffset)
+          })
+        })
+      } catch (e) {
+        throw e
+      }
+    })
+
+    function createTestName() {
+      const booleanPadMaxLength = 5
+      const textPadMaxLength = Math.max('text'.length, text.length) + 3
+      const testName =
+        [
+          'C'.padEnd(booleanPadMaxLength, ' '),
+          'W'.padEnd(booleanPadMaxLength, ' '),
+          'R'.padEnd(booleanPadMaxLength, ' '),
+          'text'.padEnd(textPadMaxLength, ' '),
+          'innerHTML',
+        ].join('  ') +
         '\n    ' +
         [
           String(shouldMatchCase).padEnd(booleanPadMaxLength, ' '),
           String(shouldMatchWholeWord).padEnd(booleanPadMaxLength, ' '),
           String(shouldUseRegularExpression).padEnd(booleanPadMaxLength, ' '),
-          JSON.stringify(text).padEnd(textPadMaxLength, ' '),
-          JSON.stringify(documentElement.querySelector('body')!.innerHTML),
+          ("'" + text + "'").padEnd(textPadMaxLength, ' '),
+          "'" + documentElement.querySelector('body')!.innerHTML + "'",
         ].join('  ') +
-        '\n   ',
-      async () => {
-        let onComplete = () => {}
-        const promise = new Promise<void>((r) => (onComplete = r))
-
-        const actual: Range[][] = []
-        const onNext = (ranges: Range[]) => actual.push(ranges)
-
-        find({
-          documentElement,
-          text,
-          shouldMatchCase,
-          shouldMatchWholeWord,
-          shouldUseRegularExpression,
-          onNext,
-          onComplete,
-        })
-        await promise
-
-        try {
-          expect(actual.length).toBe(expected.length)
-
-          actual.forEach((actualRanges, rangesListIndex) => {
-            const expectedRanges = expected[rangesListIndex]
-            expect(actualRanges.length).toBe(expectedRanges.length)
-
-            actualRanges.forEach((actualRange, rangesIndex) => {
-              const expectedRange = expectedRanges[rangesIndex]
-              expect(actualRange.startContainer).toBe(
-                expectedRange.startContainer,
-              )
-              expect(actualRange.startOffset).toBe(expectedRange.startOffset)
-              expect(actualRange.endContainer).toBe(expectedRange.endContainer)
-              expect(actualRange.endOffset).toBe(expectedRange.endOffset)
-            })
-          })
-        } catch (e) {
-          throw e
-        }
-      },
-    )
+        '\n   '
+      return testName
+    }
   },
 )
 
