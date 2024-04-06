0d80e63b8a43e03d2311abcfb8daabdc4dd70251
Ernest <iendeavor.github.io@gmail.com>
2024-04-06 12:55:17 +0800

    test: text-transform

--- src/pages/content/use-cases/__tests__/find.e2e.test.ts
+++ src/pages/content/use-cases/__tests__/find.e2e.test.ts
@@ -77,6 +77,18 @@ const suits: Suit[] = []
     expected: [],
   })
 }
+// {
+//   // TODO: we cannot highlight input value, because its textContent is empty, but is there a workaround
+//   const documentElement = createDocumentElement(`<input value="a">`)
+//   suits.push({
+//     documentElement,
+//     text: 'a',
+//     shouldMatchCase: false,
+//     shouldMatchWholeWord: false,
+//     shouldUseRegularExpression: false,
+//     expected: [],
+//   })
+// }
 // === element end ====
 
 // === class start ====
@@ -123,108 +135,231 @@ const suits: Suit[] = []
   })
 }
 {
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/text-transform
   {
     const documentElement = createDocumentElement(
-      `<span style="text-transform: none;">a</span>`,
+      `<span style="text-transform: none;">Lorem ipsum dolor sit amet, consectetur adipisicing elit…</span>`,
     )
     suits.push({
       documentElement,
-      text: 'A',
-      shouldMatchCase: true,
-      shouldMatchWholeWord: false,
-      shouldUseRegularExpression: false,
-      expected: [],
-    })
-  }
-  {
-    const documentElement = createDocumentElement(
-      `<span style="text-transform: uppercase;">a</span>`,
-    )
-    suits.push({
-      documentElement,
-      text: 'A',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit…',
       shouldMatchCase: true,
       shouldMatchWholeWord: false,
       shouldUseRegularExpression: false,
       expected: [
-        [
-          createRange(
-            documentElement.querySelector('body')!.childNodes[0].childNodes[0],
-            0,
-            1,
+        Array(57)
+          .fill(null)
+          .map((_, i) =>
+            createRange(
+              documentElement.querySelector('body')!.childNodes[0]
+                .childNodes[0],
+              i,
+              i + 1,
+            ),
           ),
-        ],
       ],
     })
   }
   {
-    const documentElement = createDocumentElement(
-      `<span style="text-transform: lowercase;">A</span>`,
-    )
-    suits.push({
-      documentElement,
-      text: 'a',
-      shouldMatchCase: true,
-      shouldMatchWholeWord: false,
-      shouldUseRegularExpression: false,
-      expected: [
-        [
-          createRange(
-            documentElement.querySelector('body')!.childNodes[0].childNodes[0],
-            0,
-            1,
-          ),
+    {
+      const documentElement = createDocumentElement(
+        `<span style="text-transform: capitalize;">(this) "is" [a] –short– -test- «for» *the* _css_ ¿capitalize? ?¡transform!</span>`,
+      )
+      suits.push({
+        documentElement,
+        text: '(This) "Is" [A] –Short– -Test- «For» *The* _css_ ¿Capitalize? ?¡Transform!',
+        shouldMatchCase: true,
+        shouldMatchWholeWord: false,
+        shouldUseRegularExpression: false,
+        expected: [
+          Array(74)
+            .fill(null)
+            .map((_, i) =>
+              createRange(
+                documentElement.querySelector('body')!.childNodes[0]
+                  .childNodes[0],
+                i,
+                i + 1,
+              ),
+            ),
         ],
-      ],
-    })
+      })
+    }
+    // TODO:
+    // {
+    //   const documentElement = createDocumentElement(
+    //     `<span style="text-transform: capitalize;">ⓐⓑⓒ (ⓓⓔⓕ) —ⓖⓗⓘ— ⓙkl</span>`,
+    //   )
+    //   suits.push({
+    //     documentElement,
+    //     text: 'Ⓐⓑⓒ (Ⓓⓔⓕ) —Ⓖⓗⓘ— Ⓙkl',
+    //     shouldMatchCase: true,
+    //     shouldMatchWholeWord: false,
+    //     shouldUseRegularExpression: false,
+    //     expected: [
+    //       Array(19)
+    //         .fill(null)
+    //         .map((_, i) =>
+    //           createRange(
+    //             documentElement.querySelector('body')!.childNodes[0]
+    //               .childNodes[0],
+    //             i,
+    //             i + 1,
+    //           ),
+    //         ),
+    //     ],
+    //   })
+    // }
+    {
+      const documentElement = createDocumentElement(
+        `<span style="text-transform: capitalize;">The Dutch word: "ijsland" starts with a digraph.</span>`,
+      )
+      suits.push({
+        documentElement,
+        text: 'The Dutch Word: "Ijsland" Starts With A Digraph.',
+        shouldMatchCase: true,
+        shouldMatchWholeWord: false,
+        shouldUseRegularExpression: false,
+        expected: [
+          Array(48)
+            .fill(null)
+            .map((_, i) =>
+              createRange(
+                documentElement.querySelector('body')!.childNodes[0]
+                  .childNodes[0],
+                i,
+                i + 1,
+              ),
+            ),
+        ],
+      })
+    }
   }
   {
-    const documentElement = createDocumentElement(
-      `<span style="text-transform: capitalize;">aBc aBc</span>`,
-    )
-    suits.push({
-      documentElement,
-      text: 'ABc',
-      shouldMatchCase: true,
-      shouldMatchWholeWord: false,
-      shouldUseRegularExpression: false,
-      expected: [
-        [
-          createRange(
-            documentElement.querySelector('body')!.childNodes[0].childNodes[0],
-            0,
-            1,
-          ),
-          createRange(
-            documentElement.querySelector('body')!.childNodes[0].childNodes[0],
-            1,
-            2,
-          ),
-          createRange(
-            documentElement.querySelector('body')!.childNodes[0].childNodes[0],
-            2,
-            3,
-          ),
+    {
+      const documentElement = createDocumentElement(
+        `<span style="text-transform: uppercase;">Lorem ipsum dolor sit amet, consectetur adipisicing elit…</span>`,
+      )
+      suits.push({
+        documentElement,
+        text: 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT…',
+        shouldMatchCase: true,
+        shouldMatchWholeWord: false,
+        shouldUseRegularExpression: false,
+        expected: [
+          Array(57)
+            .fill(null)
+            .map((_, i) =>
+              createRange(
+                documentElement.querySelector('body')!.childNodes[0]
+                  .childNodes[0],
+                i,
+                i + 1,
+              ),
+            ),
         ],
-        [
-          createRange(
-            documentElement.querySelector('body')!.childNodes[0].childNodes[0],
-            4,
-            5,
-          ),
-          createRange(
-            documentElement.querySelector('body')!.childNodes[0].childNodes[0],
-            5,
-            6,
-          ),
-          createRange(
-            documentElement.querySelector('body')!.childNodes[0].childNodes[0],
-            6,
-            7,
-          ),
+      })
+    }
+    // TODO:
+    // {
+    //   const documentElement = createDocumentElement(
+    //     `<span style="text-transform: uppercase;">Θα πάμε στο "Θεϊκό φαΐ" ή στη "Νεράιδα"</span>`,
+    //   )
+    //   suits.push({
+    //     documentElement,
+    //     text: 'ΘΑ ΠΆΜΕ ΣΤΟ "ΘΕΪΚΌ ΦΑΪ́" Ή ΣΤΗ "ΝΕΡΆΙΔΑ"',
+    //     shouldMatchCase: true,
+    //     shouldMatchWholeWord: false,
+    //     shouldUseRegularExpression: false,
+    //     expected: [
+    //       Array(39)
+    //         .fill(null)
+    //         .map((_, i) =>
+    //           createRange(
+    //             documentElement.querySelector('body')!.childNodes[0]
+    //               .childNodes[0],
+    //             i,
+    //             i + 1,
+    //           ),
+    //         ),
+    //     ],
+    //   })
+    // }
+  }
+  {
+    {
+      const documentElement = createDocumentElement(
+        `<span style="text-transform: lowercase;">Lorem ipsum dolor sit amet, consectetur adipisicing elit…</span>`,
+      )
+      suits.push({
+        documentElement,
+        text: 'lorem ipsum dolor sit amet, consectetur adipisicing elit…',
+        shouldMatchCase: true,
+        shouldMatchWholeWord: false,
+        shouldUseRegularExpression: false,
+        expected: [
+          Array(57)
+            .fill(null)
+            .map((_, i) =>
+              createRange(
+                documentElement.querySelector('body')!.childNodes[0]
+                  .childNodes[0],
+                i,
+                i + 1,
+              ),
+            ),
         ],
-      ],
-    })
+      })
+    }
+    // TODO:
+    // {
+    //   const documentElement = createDocumentElement(
+    //     `<span style="text-transform: lowercase;">Σ IS A greek LETTER that appears SEVERAL TIMES IN ΟΔΥΣΣΕΥΣ.</span>`,
+    //   )
+    //   suits.push({
+    //     documentElement,
+    //     text: 'σ is a greek letter that appears several times in οδυσσευς.',
+    //     shouldMatchCase: true,
+    //     shouldMatchWholeWord: false,
+    //     shouldUseRegularExpression: false,
+    //     expected: [
+    //       Array(59)
+    //         .fill(null)
+    //         .map((_, i) =>
+    //           createRange(
+    //             documentElement.querySelector('body')!.childNodes[0]
+    //               .childNodes[0],
+    //             i,
+    //             i + 1,
+    //           ),
+    //         ),
+    //     ],
+    //   })
+    // }
+    {
+      const documentElement = createDocumentElement(
+        `<span style="text-transform: lowercase;">Ĩ is a Lithuanian LETTER as is J́</span>`,
+      )
+      suits.push({
+        documentElement,
+        text: 'ĩ is a lithuanian letter as is j́',
+        shouldMatchCase: true,
+        shouldMatchWholeWord: false,
+        shouldUseRegularExpression: false,
+        expected: [
+          Array(33)
+            .fill(null)
+            .map((_, i) =>
+              createRange(
+                documentElement.querySelector('body')!.childNodes[0]
+                  .childNodes[0],
+                i,
+                i + 1,
+              ),
+            ),
+        ],
+      })
+    }
   }
 }
 // === style end ====
@@ -418,17 +553,21 @@ suits.forEach(
           'C'.padEnd(booleanPadMaxLength, ' '),
           'W'.padEnd(booleanPadMaxLength, ' '),
           'R'.padEnd(booleanPadMaxLength, ' '),
-          'text'.padEnd(textPadMaxLength, ' '),
-          'innerHTML',
         ].join('  ') +
         '\n    ' +
         [
           String(shouldMatchCase).padEnd(booleanPadMaxLength, ' '),
           String(shouldMatchWholeWord).padEnd(booleanPadMaxLength, ' '),
           String(shouldUseRegularExpression).padEnd(booleanPadMaxLength, ' '),
-          ("'" + text + "'").padEnd(textPadMaxLength, ' '),
-          "'" + documentElement.querySelector('body')!.innerHTML + "'",
         ].join('  ') +
+        '\n    ' +
+        'text      : `' +
+        text +
+        '`' +
+        '\n    ' +
+        'innerHTML : `' +
+        documentElement.innerHTML +
+        '`' +
         '\n   '
       return testName
     }
