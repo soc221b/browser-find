da7e0e78bc5d3549993c16e97ce1519adf2b4461
Ernest <iendeavor.github.io@gmail.com>
2024-04-07 13:38:24 +0800

    test: `<span>a</span>\n<span>b</span>`

--- src/pages/content/use-cases/__tests__/find.e2e.test.ts
+++ src/pages/content/use-cases/__tests__/find.e2e.test.ts
@@ -1068,6 +1068,33 @@ const suits: Suit[] = []
     ],
   })
 }
+{
+  const documentElement = createDocumentElement(
+    `<span>a</span>\n<span>b</span>`,
+  )
+  suits.push({
+    documentElement,
+    text: 'a b',
+    shouldMatchCase: false,
+    shouldMatchWholeWord: false,
+    shouldUseRegularExpression: false,
+    expected: [
+      [
+        createRange(
+          documentElement.querySelector('body')!.childNodes[0].childNodes[0],
+          0,
+          1,
+        ),
+        createRange(documentElement.querySelector('body')!.childNodes[1], 0, 1),
+        createRange(
+          documentElement.querySelector('body')!.childNodes[2].childNodes[0],
+          0,
+          1,
+        ),
+      ],
+    ],
+  })
+}
 // === spaces end ====
 
 suits.forEach(
