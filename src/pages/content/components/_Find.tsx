50745958e865011e053f36b235e22f119fa588af
Ernest <iendeavor.github.io@gmail.com>
2024-04-08 11:20:07 +0800

    refactor: no need to destruct

--- src/pages/content/components/_Find.tsx
+++ src/pages/content/components/_Find.tsx
@@ -4,40 +4,33 @@ import useStore from '../store'
 import { highlights } from '../utils/highlights'
 
 export default function _Find(): JSX.Element {
-  const dispatch = useStore((state) => state.dispatch)
-  const open = useStore((state) => state.open)
-  const shouldMatchCase = useStore((state) => state.shouldMatchCase)
-  const shouldMatchWholeWord = useStore((state) => state.shouldMatchWholeWord)
-  const shouldUseRegularExpression = useStore((state) => state.shouldUseRegularExpression)
-  const text = useStore((state) => state.text)
-  const matches = useStore((state) => state.matches)
-  const matchId = useStore((state) => state.matchId)
+  const store = useStore()
   const [isCompleted, setIsCompleted] = useState(false)
 
   useEffect(() => {
-    matches.forEach((match) => {
+    store.matches.forEach((match) => {
       match.ranges.forEach((range) => {
         highlights({ range, isAdd: false, isThis: true })
         highlights({ range, isAdd: false, isThis: false })
       })
     })
-    dispatch({ type: 'ClearMatch' })
-  }, [shouldMatchCase, shouldMatchWholeWord, shouldUseRegularExpression, text])
+    store.dispatch({ type: 'ClearMatch' })
+  }, [store.shouldMatchCase, store.shouldMatchWholeWord, store.shouldUseRegularExpression, store.text])
 
   useEffect(() => {
-    if (open === false) {
+    if (store.open === false) {
       return
     }
 
     setIsCompleted(false)
     const { stop } = find({
       documentElement: document.documentElement,
-      text,
-      shouldMatchCase,
-      shouldMatchWholeWord,
-      shouldUseRegularExpression,
+      text: store.text,
+      shouldMatchCase: store.shouldMatchCase,
+      shouldMatchWholeWord: store.shouldMatchWholeWord,
+      shouldUseRegularExpression: store.shouldUseRegularExpression,
       onNext: (ranges) => {
-        dispatch({
+        store.dispatch({
           type: 'Match',
           match: { id: Math.random().toString(36), ranges },
         })
@@ -50,18 +43,18 @@ export default function _Find(): JSX.Element {
     return () => {
       stop()
     }
-  }, [shouldMatchCase, shouldMatchWholeWord, shouldUseRegularExpression, text])
+  }, [store.shouldMatchCase, store.shouldMatchWholeWord, store.shouldUseRegularExpression, store.text])
 
   useEffect(() => {
     if (isCompleted === false) {
       return
     }
 
-    matches.forEach((match) => {
+    store.matches.forEach((match) => {
       match.ranges.forEach((range, index) => {
         highlights({ range, isAdd: false, isThis: true })
         highlights({ range, isAdd: false, isThis: false })
-        if (match.id === matchId) {
+        if (match.id === store.matchId) {
           highlights({ range, isAdd: true, isThis: true })
           if (index === 0) {
             range.startContainer.parentElement?.scrollIntoView({
@@ -75,7 +68,7 @@ export default function _Find(): JSX.Element {
         }
       })
     })
-  }, [isCompleted, matches, matchId])
+  }, [isCompleted, store.matches, store.matchId])
 
   return <></>
 }
