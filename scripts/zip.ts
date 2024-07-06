51ea75b4b8913511661ea00e48376d2540ae3f00
Ernest <>
2024-07-06 12:35:54 +0800

    build: zip

---
+++ scripts/zip.ts
@@ -0,0 +1,11 @@
+import { sync as rimrafSync } from 'rimraf'
+import { zipSync } from 'cross-zip'
+
+function build() {
+  rimrafSync('dist.zip')
+  zipSync('dist/v3', '../dist.zip')
+
+  console.log('Finished.')
+}
+
+build()
