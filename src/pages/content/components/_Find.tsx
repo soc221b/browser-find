ad8760786ff7889ab4bebf079ce7d8be8ca1bdd5
Ernest <>
2024-07-11 22:11:21 +0800

    fix: page should not shift when navigating with fragments
    reverts: 0d8952c, 40a6c5a

--- src/pages/content/components/_Find.tsx
+++ src/pages/content/components/_Find.tsx
@@ -1,27 +1,9 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { find } from '../use-cases/find'
 import useStore from '../store'
-import useToggle from '../hooks/use-toggle'
 
 export default function _Find(): JSX.Element {
   const store = useStore()
-
-  const [forceFindFlag, toggleForceFindFlag] = useToggle()
-  const hrefWithoutHash = useRef(getHrefWithoutHash(location.href))
-  useEffect(() => {
-    ;(window as any).navigation?.addEventListener('navigate', handleNavigate)
-    return () => {
-      ;(window as any).navigation?.removeEventListener('navigate', handleNavigate)
-    }
-
-    function handleNavigate() {
-      const currentHrefWithoutHash = getHrefWithoutHash(location.href)
-      if (currentHrefWithoutHash === hrefWithoutHash.current) return
-
-      hrefWithoutHash.current = currentHrefWithoutHash
-      toggleForceFindFlag()
-    }
-  }, [forceFindFlag])
 
   useEffect(() => {
     store.dispatch({ type: 'ClearMatch' })
@@ -47,13 +29,7 @@ export default function _Find(): JSX.Element {
     return () => {
       stop()
     }
-  }, [store.shouldMatchCase, store.shouldMatchWholeWord, store.shouldUseRegularExpression, store.text, forceFindFlag])
+  }, [store.shouldMatchCase, store.shouldMatchWholeWord, store.shouldUseRegularExpression, store.text])
 
   return <></>
-}
-
-function getHrefWithoutHash(href: string): string {
-  const url = new URL(href)
-  url.hash = ''
-  return url.href
 }
