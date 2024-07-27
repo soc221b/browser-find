2c8ccbab138650566859c7afc941b7a8e31be09b
Ernest <>
2024-07-27 14:37:40 +0800

    refactor: simplify

--- src/pages/content/components/_ActionIcon.tsx
+++ src/pages/content/components/_ActionIcon.tsx
@@ -3,11 +3,7 @@ import usePrefersColorScheme from '../hooks/use-prefers-color-scheme'
 export default function _ActionIcon(): JSX.Element {
   const prefersColorScheme = usePrefersColorScheme()
 
-  if (prefersColorScheme === 'dark') {
-    chrome.runtime.sendMessage({ action: 'toggle-icon', scheme: 'dark' })
-  } else {
-    chrome.runtime.sendMessage({ action: 'toggle-icon', scheme: 'default' })
-  }
+  chrome.runtime.sendMessage({ action: 'toggle-icon', scheme: prefersColorScheme })
 
   return <></>
 }
