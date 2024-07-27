e3f1f8977ee8d40ba8b54fe75f2934ec2434512e
Ernest <>
2024-07-27 11:04:16 +0800

    refactor: rearrange

--- scripts/zip.ts
+++ scripts/zip.ts
@@ -1,11 +1,11 @@
 import { sync as rimrafSync } from 'rimraf'
 import { zipSync } from 'cross-zip'
 
+main()
+
 function main() {
   rimrafSync('dist.zip')
   zipSync('dist/v3', '../dist.zip')
 
   console.log('Finished.')
 }
-
-main()
